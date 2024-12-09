0ed370a4e9a9374293f9dc77c076ba6721f63705
hasecilu <hasecilu@tuta.io>
2024-12-08 22:43:05 -0600

    Use translate functions from `FreeCAD.Qt` module

--- SteelColumnGui.py
+++ SteelColumnGui.py
@@ -1,60 +1,47 @@
-from PySide import QtCore
+from pathlib import Path
+
 import FreeCAD
 import FreeCADGui
-import column_types
+
 import column_type
+import column_types
 import section
 import update
 
-from pathlib import Path
-
 steel_column_path = Path(__file__).parent
 
-
-def QT_TRANSLATE_NOOP(ctx, txt): return txt
+QT_TRANSLATE_NOOP = FreeCAD.Qt.QT_TRANSLATE_NOOP
 
 
 class Dxf:
     def Activated(self):
-        filename = get_save_filename('.dxf')
+        filename = get_save_filename(".dxf")
         if not filename:
             return
         import techdraw
+
         techdraw.export_to_dxf(filename)
 
     def IsActive(self):
         return True if FreeCADGui.ActiveDocument else False
 
     def GetResources(self):
-        MenuText = QtCore.QT_TRANSLATE_NOOP(
-            "Dxf",
-            "Export to Dxf")
-        ToolTip = QtCore.QT_TRANSLATE_NOOP(
-            "Dxf",
-            "Export to Dxf")
-        
-        path = steel_column_path / 'Resources' / 'icons' / 'Dxf.svg'
-        return {'Pixmap': str(path),
-                'MenuText': MenuText,
-                'ToolTip': ToolTip}
+        MenuText = QT_TRANSLATE_NOOP("Dxf", "Export to DXF")
+        ToolTip = QT_TRANSLATE_NOOP("Dxf", "Export to DXF")
+
+        path = steel_column_path / "Resources" / "icons" / "Dxf.svg"
+        return {"Pixmap": str(path), "MenuText": MenuText, "ToolTip": ToolTip}
+
 
 class Levels:
-
     def GetResources(self):
-        MenuText = QtCore.QT_TRANSLATE_NOOP(
-            "Levels",
-            "Creates Levels")
-        ToolTip = QtCore.QT_TRANSLATE_NOOP(
-            "Levels",
-            "Creates Levels")
-        
-        path = steel_column_path / 'Resources' / 'icons' / 'Levels.svg'
-        return {'Pixmap': str(path),
-                'MenuText': MenuText,
-                'ToolTip': ToolTip}
+        MenuText = QT_TRANSLATE_NOOP("Levels", "Creates Levels")
+        ToolTip = QT_TRANSLATE_NOOP("Levels", "Creates Levels")
 
+        path = steel_column_path / "Resources" / "icons" / "Levels.svg"
+        return {"Pixmap": str(path), "MenuText": MenuText, "ToolTip": ToolTip}
 
-    def  Activated(self):
+    def Activated(self):
         column_types.create_levels()
 
     def IsActive(self):
@@ -62,22 +49,14 @@ class Levels:
 
 
 class Columns:
-
     def GetResources(self):
-        MenuText = QtCore.QT_TRANSLATE_NOOP(
-            "Levels",
-            "Creates Columns")
-        ToolTip = QtCore.QT_TRANSLATE_NOOP(
-            "Levels",
-            "Creates Columns")
-        
-        path = steel_column_path / 'Resources' / 'icons' / 'add.svg'
-        return {'Pixmap': str(path),
-                'MenuText': MenuText,
-                'ToolTip': ToolTip}
+        MenuText = QT_TRANSLATE_NOOP("Levels", "Creates Columns")
+        ToolTip = QT_TRANSLATE_NOOP("Levels", "Creates Columns")
 
+        path = steel_column_path / "Resources" / "icons" / "add.svg"
+        return {"Pixmap": str(path), "MenuText": MenuText, "ToolTip": ToolTip}
 
-    def  Activated(self):
+    def Activated(self):
         column_type.create_columns()
 
     def IsActive(self):
@@ -86,20 +65,13 @@ class Columns:
 
 class RemoveColumn:
     def GetResources(self):
-        MenuText = QtCore.QT_TRANSLATE_NOOP(
-            "Levels",
-            "Remove Columns")
-        ToolTip = QtCore.QT_TRANSLATE_NOOP(
-            "Levels",
-            "Remove Columns")
-        
-        path = steel_column_path / 'Resources' / 'icons' / 'remove.svg'
-        return {'Pixmap': str(path),
-                'MenuText': MenuText,
-                'ToolTip': ToolTip}
+        MenuText = QT_TRANSLATE_NOOP("Levels", "Remove Columns")
+        ToolTip = QT_TRANSLATE_NOOP("Levels", "Remove Columns")
 
+        path = steel_column_path / "Resources" / "icons" / "remove.svg"
+        return {"Pixmap": str(path), "MenuText": MenuText, "ToolTip": ToolTip}
 
-    def  Activated(self):
+    def Activated(self):
         column_type.remove_column()
 
     def IsActive(self):
@@ -107,22 +79,14 @@ class RemoveColumn:
 
 
 class Sections:
-
     def GetResources(self):
-        MenuText = QtCore.QT_TRANSLATE_NOOP(
-            "Sections",
-            "Creates Sections")
-        ToolTip = QtCore.QT_TRANSLATE_NOOP(
-            "Sections",
-            "Creates Sections")
-        
-        path = steel_column_path / 'Resources' / 'icons' / 'section.svg'
-        return {'Pixmap': str(path),
-                'MenuText': MenuText,
-                'ToolTip': ToolTip}
+        MenuText = QT_TRANSLATE_NOOP("Sections", "Creates Sections")
+        ToolTip = QT_TRANSLATE_NOOP("Sections", "Creates Sections")
 
+        path = steel_column_path / "Resources" / "icons" / "section.svg"
+        return {"Pixmap": str(path), "MenuText": MenuText, "ToolTip": ToolTip}
 
-    def  Activated(self):
+    def Activated(self):
         section.create_sections()
 
     def IsActive(self):
@@ -130,37 +94,28 @@ class Sections:
 
 
 class Update:
-
     def GetResources(self):
-        MenuText = QtCore.QT_TRANSLATE_NOOP(
-            "Update",
-            "Update")
-        ToolTip = QtCore.QT_TRANSLATE_NOOP(
-            "Update",
-            "Update Steel Column WorkBench")
-        
-        path = steel_column_path / 'Resources' / 'icons' / 'update.png'
-        return {'Pixmap': str(path),
-                'MenuText': MenuText,
-                'ToolTip': ToolTip}
+        MenuText = QT_TRANSLATE_NOOP("Update", "Update")
+        ToolTip = QT_TRANSLATE_NOOP("Update", "Update Steel Column WorkBench")
 
+        path = steel_column_path / "Resources" / "icons" / "update.png"
+        return {"Pixmap": str(path), "MenuText": MenuText, "ToolTip": ToolTip}
 
-    def  Activated(self):
+    def Activated(self):
         update.update()
 
     def IsActive(self):
         return True
 
 
-
 def get_save_filename(ext):
     from PySide.QtWidgets import QFileDialog
+
     filters = f"{ext[1:]} (*{ext})"
-    filename, _ = QFileDialog.getSaveFileName(None, 'select file',
-                                              None, filters)
+    filename, _ = QFileDialog.getSaveFileName(None, "select file", None, filters)
     if not filename:
         return
-    if not ext in filename:
+    if ext not in filename:
         filename += ext
     return filename
 
@@ -169,7 +124,7 @@ FreeCADGui.addCommand("steel_column_levels", Levels())
 FreeCADGui.addCommand("steel_column_section", Sections())
 FreeCADGui.addCommand("steel_column_columns", Columns())
 FreeCADGui.addCommand("steel_column_remove", RemoveColumn())
-FreeCADGui.addCommand('steel_column_dxf', Dxf())
+FreeCADGui.addCommand("steel_column_dxf", Dxf())
 FreeCADGui.addCommand("steel_column_update", Update())
 
 steel_column_commands = [
@@ -179,4 +134,4 @@ steel_column_commands = [
     "steel_column_remove",
     "steel_column_dxf",
     "steel_column_update",
-    ]
+]
