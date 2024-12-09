905024f5c6dfe7582b636778a0229007e96e847d
hasecilu <hasecilu@tuta.io>
2024-12-08 23:50:54 -0600

    Add Spanish translations

---
+++ Resources/translations/SteelColumn_es-ES.ts
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en_US">
+<context>
+    <name>Columns</name>
+    <message>
+        <location filename="../ui/column.ui" line="14"/>
+        <source>Columns</source>
+        <translation>Columnas</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="118"/>
+        <source>conn. ipe len</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="147"/>
+        <source>conn. ipe above</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="157"/>
+        <source>IPE size</source>
+        <translation>Tamaño IPE</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="164"/>
+        <source>N</source>
+        <translation>N</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="209"/>
+        <source>dx</source>
+        <translation>dx</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="248"/>
+        <source>extend plate down</source>
+        <translation>Extender placa hacia abajo</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="255"/>
+        <source>extend plate up</source>
+        <translation>Extender placa hacia arriba</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="262"/>
+        <source>extend length</source>
+        <translation>Extender longitud</translation>
+    </message>
+    <message>
+        <location filename="../ui/column.ui" line="288"/>
+        <source>extend 3IPE below</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Sections</name>
+    <message>
+        <location filename="../ui/sections.ui" line="14"/>
+        <source>Sections</source>
+        <translation>Secciones</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="29"/>
+        <source>Add</source>
+        <translation>Agregar</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="36"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="45"/>
+        <source>number</source>
+        <translation>Número</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="52"/>
+        <source>size</source>
+        <translation>Tamaño</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="119"/>
+        <source>Flang Plate</source>
+        <translation>Placa de brida</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="131"/>
+        <location filename="../ui/sections.ui" line="186"/>
+        <location filename="../ui/sections.ui" line="241"/>
+        <source>b</source>
+        <translation>b</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="151"/>
+        <location filename="../ui/sections.ui" line="206"/>
+        <location filename="../ui/sections.ui" line="261"/>
+        <source>t</source>
+        <translation>t</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="174"/>
+        <source>Web Plate</source>
+        <translation>Placa web</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="229"/>
+        <source>Side Plate</source>
+        <translation>Placa lateral</translation>
+    </message>
+    <message>
+        <location filename="../ui/sections.ui" line="284"/>
+        <source>pa baz</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SteelColumn_CreateColumns</name>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="54"/>
+        <location filename="../../SteelColumnGui.py" line="56"/>
+        <source>Creates Columns</source>
+        <translation>Crear columnas</translation>
+    </message>
+</context>
+<context>
+    <name>SteelColumn_CreateLevels</name>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="39"/>
+        <location filename="../../SteelColumnGui.py" line="41"/>
+        <source>Creates Levels</source>
+        <translation>Crear niveles</translation>
+    </message>
+</context>
+<context>
+    <name>SteelColumn_CreateSections</name>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="84"/>
+        <location filename="../../SteelColumnGui.py" line="86"/>
+        <source>Creates Sections</source>
+        <translation>Crear secciones</translation>
+    </message>
+</context>
+<context>
+    <name>SteelColumn_ExportDxf</name>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="30"/>
+        <location filename="../../SteelColumnGui.py" line="32"/>
+        <source>Export to DXF</source>
+        <translation>Exportar a DXF</translation>
+    </message>
+</context>
+<context>
+    <name>SteelColumn_RemoveColumns</name>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="69"/>
+        <location filename="../../SteelColumnGui.py" line="71"/>
+        <source>Remove Columns</source>
+        <translation>Eliminar columnas</translation>
+    </message>
+</context>
+<context>
+    <name>SteelColumn_UpdateWorkbench</name>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="99"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../../SteelColumnGui.py" line="101"/>
+        <source>Update Steel Column WorkBench</source>
+        <translation>Actualizar banco de trabajo Steel Column</translation>
+    </message>
+</context>
+<context>
+    <name>Stories</name>
+    <message>
+        <location filename="../ui/story.ui" line="14"/>
+        <source>Stories</source>
+        <translation>Pisos</translation>
+    </message>
+    <message>
+        <location filename="../ui/story.ui" line="22"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../ui/story.ui" line="33"/>
+        <source>Add      </source>
+        <translation>Agregar...</translation>
+    </message>
+    <message>
+        <location filename="../ui/story.ui" line="50"/>
+        <source>Height</source>
+        <translation>Altura</translation>
+    </message>
+    <message>
+        <location filename="../ui/story.ui" line="57"/>
+        <source>Vertical Scale</source>
+        <translation>Escala vertical</translation>
+    </message>
+    <message>
+        <location filename="../ui/story.ui" line="107"/>
+        <source>composite Deck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/story.ui" line="119"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../../InitGui.py" line="23"/>
+        <location filename="../../InitGui.py" line="33"/>
+        <source>SteelColumn</source>
+        <translation>SteelColumn</translation>
+    </message>
+    <message>
+        <location filename="../../InitGui.py" line="25"/>
+        <source>SteelColumn Workbench</source>
+        <translation>Banco de trabajo Steel Column</translation>
+    </message>
+    <message>
+        <location filename="../../InitGui.py" line="32"/>
+        <source>SteelColumn tools</source>
+        <translation>Herramientas de SteelColumn</translation>
+    </message>
+</context>
+</TS>
